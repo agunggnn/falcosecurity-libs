c4dc1d022fe032464c266fc9e7a8b8e6bcab1ca5
agunggnn <43904207+agunggnn@users.noreply.github.com>
2021-07-09 17:33:40 +0700

    Update k8s_net.cpp

--- userspace/libsinsp/k8s_net.cpp
+++ userspace/libsinsp/k8s_net.cpp
@@ -159,7 +159,8 @@ bool k8s_net::has_dependency(const k8s_component::type_map::value_type& componen
 
 k8s_net::handler_ptr_t k8s_net::make_handler(k8s_state_t& state, const k8s_component::type component, bool connect,
 											handler_ptr_t dep, collector_ptr_t collector, const std::string& urlstr,
-											ssl_ptr_t ssl, bt_ptr_t bt, bool blocking, filter_ptr_t event_filter)
+											ssl_ptr_t ssl, bt_ptr_t bt, bool blocking, filter_ptr_t event_filter,
+											const std::string& node_selector)
 {
 	switch(component)
 	{
@@ -168,7 +169,7 @@ k8s_net::handler_ptr_t k8s_net::make_handler(k8s_state_t& state, const k8s_compo
 		case k8s_component::K8S_NAMESPACES:
 			return std::make_shared<k8s_namespace_handler>(state, dep, collector, urlstr, "1.1", ssl, bt, connect, blocking);
 		case k8s_component::K8S_PODS:
-			return std::make_shared<k8s_pod_handler>(state, dep, collector, urlstr, "1.1", ssl, bt, connect, blocking);
+			return std::make_shared<k8s_pod_handler>(state, dep, collector, urlstr, "1.1", ssl, bt, connect, blocking, node_selector);
 		case k8s_component::K8S_REPLICATIONCONTROLLERS:
 			return std::make_shared<k8s_replicationcontroller_handler>(state, dep, collector, urlstr, "1.1", ssl, bt, connect, blocking);
 		case k8s_component::K8S_REPLICASETS:
@@ -195,7 +196,7 @@ void k8s_net::add_handler(const k8s_component::type_map::value_type& component)
 	{
 		handler_ptr_t handler =
 			make_handler(m_state, component.first, true, get_dependency_handler(m_handlers, component),
-						 m_collector, m_uri.to_string(), m_ssl, m_bt, m_blocking_sockets, m_event_filter);
+						 m_collector, m_uri.to_string(), m_ssl, m_bt, m_blocking_sockets, m_event_filter, m_node_selector);
 		if(handler)
 		{
 			if(!m_machine_id.empty())
